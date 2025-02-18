1cb2cb35e5841bba5042359b118a4959cba5d39d
VarunR1 <varunteja4369@gmail.com>
2025-02-17 21:29:20 -0600

    Create storedp_load_bronze.sql

---
+++ scripts/storedp_load_bronze.sql
@@ -0,0 +1,142 @@
+/*
+-----------------------------------------------------
+Creating Stored Procedure for Bronze Layer.
+-----------------------------------------------------
+Purpose of the Script:
+  The main purpose of this stored procedure is to load the data into the 'bronze' schema from external CSV files. 
+The script have perfomed the following actions:
+  1- Truncates the bronze tables before loading. 
+  2- Used the 'Bulk Insert' command to load the data from CSV files to bronze tables.
+
+Stored Procedure example query: Execute bronze.load_bronze;
+------------------------------------------------------
+*/
+
+Create or Alter Procedure bronze.load_bronze As
+Begin
+	Declare @start_time DateTime, @end_time DateTime, @batch_start_time DateTime, @batch_end_time DateTime;
+	Begin Try
+  	Set @batch_start_time = GetDate();
+  		Print Replicate('-',40);
+  		Print'Starting Bronze Layer Data Load';
+  		Print Replicate('-', 40);
+  
+  		Print Replicate('=', 40);
+  		print('Loading the CRM Tables');
+  		Print Replicate ('=', 40);
+
+	  Set @start_time = GetDate();
+			Print'>>> Truncating Table: bronze.crm_cust_info';
+			Truncate Table bronze.crm_cust_info;
+			Print'>>> Inserting the Data into: bronze.crm_cust_info';
+			Bulk Insert bronze.crm_cust_info
+			From "C:\Users\lasya\OneDrive\Desktop\DW Project\dw_project\datasets\source_crm\cust_info.csv"
+			With (
+					FirstRow = 2, 
+					FieldTerminator = ',',
+					TabLock
+			);
+  	Set @end_time = GetDate();
+  		Print '>>> Load Duration: ' + Cast(DateDiff(second, @start_time, @end_time) As nvarchar) + ' seconds';
+  		Print Replicate('-', 20);
+
+
+	  Set @start_time = GetDate();
+			Print'>>> Truncating Table: bronze.crm_prd_info';
+			Truncate Table bronze.crm_prd_info;
+			Print'>>> Inserting the Data into: bronze.crm_prd_info';
+			Bulk Insert bronze.crm_prd_info
+			From "C:\Users\lasya\OneDrive\Desktop\DW Project\dw_project\datasets\source_crm\prd_info.csv"
+			With (
+					FirstRow = 2, 
+					FieldTerminator = ',',
+					TabLock
+			);
+    Set @end_time = GetDate();
+  		Print '>>> Load Duration: ' + Cast(DateDiff(second, @start_time, @end_time) As nvarchar) + ' seconds';
+  		Print Replicate('-', 20);
+
+
+	  Set @start_time = GetDate();
+			Print'>>> Truncating Table:  bronze.crm_sales_details';
+			Truncate Table bronze.crm_sales_details;
+			Print'>>> Inserting the Data into: bronze.crm_sales_details';
+			Bulk Insert bronze.crm_sales_details
+
+			From "C:\Users\lasya\OneDrive\Desktop\DW Project\dw_project\datasets\source_crm\sales_details.csv"
+			With (
+					FirstRow = 2, 
+					FieldTerminator = ',',
+					TabLock
+			);
+	  Set @end_time = GetDate();
+  		Print '>>> Load Duration: ' + Cast(DateDiff(second, @start_time, @end_time) As nvarchar) + ' seconds';
+  		Print Replicate('-', 20);
+
+		
+  		Print Replicate('=', 40);
+  		print('Loading the ERP Tables');
+  		Print Replicate ('=', 40);
+
+	  Set @start_time = GetDate();
+			Print'>>> Truncating Table: bronze.erp_cust_az12';
+			Truncate Table bronze.erp_cust_az12;
+			Print'>>> Inserting the Data into: bronze.erp_cust_az12';
+			Bulk Insert bronze.erp_cust_az12
+			From "C:\Users\lasya\OneDrive\Desktop\DW Project\dw_project\datasets\source_erp\CUST_AZ12.csv"
+			With (
+					FirstRow = 2, 
+					FieldTerminator = ',',
+					TabLock
+			);
+	  Set @end_time = GetDate();
+		Print '>>> Load Duration: ' + Cast(DateDiff(second, @start_time, @end_time) As nvarchar) + ' seconds';
+		Print Replicate('-', 20);
+
+
+	  Set @start_time = GetDate();
+			Print'>>> Truncating Table: bronze.erp_loc_a101';
+			Truncate Table bronze.erp_loc_a101;
+			Print'>>> Inserting the Data into: bronze.erp_loc_a101';
+			Bulk Insert bronze.erp_loc_a101
+			From "C:\Users\lasya\OneDrive\Desktop\DW Project\dw_project\datasets\source_erp\LOC_A101.csv"
+			With (
+					FirstRow = 2, 
+					FieldTerminator = ',',
+					TabLock
+			);
+  	Set @end_time = GetDate();
+		  Print '>>> Load Duration: ' + Cast(DateDiff(second, @start_time, @end_time) As nvarchar) + ' seconds';
+		  Print Replicate('-', 20);
+
+
+	  Set @start_time = GetDate();
+			Print'>>> Truncating Table: erp_px_cat_g1v2';
+			Truncate Table bronze.erp_px_cat_g1v2;
+			Print'>>> Inserting the Data into: bronze.erp_px_cat_g1v2';
+			Bulk Insert bronze.erp_px_cat_g1v2
+			From "C:\Users\lasya\OneDrive\Desktop\DW Project\dw_project\datasets\source_erp\PX_CAT_G1V2.csv"
+			With (
+					FirstRow = 2, 
+					FieldTerminator = ',',
+					TabLock
+			);
+	  Set @end_time = GetDate();
+	  	Print '>>> Load Duration: ' + Cast(DateDiff(second, @start_time, @end_time) As nvarchar) + ' seconds';
+		  Print Replicate('-', 20);
+
+	  Set @batch_end_time= GetDate();
+  	  Print Replicate('-', 40)
+  		Print 'Loading the Bronze layer has been Completed'
+  		Print '  -Total Load Duration: ' + Cast(DateDiff(second, @batch_start_time, @batch_end_time) As nvarchar) + ' seconds';
+  	  Print Replicate('-', 40)
+	  End Try
+  	Begin Catch
+  		Print Replicate('-',40)
+  		Print 'Error Occured During Loading the Bronze Layer'
+  		Print 'Error Message' + Error_Message();
+  		Print 'Error Message' + Cast (Error_Number() as nvarchar);
+  		Print 'Error Message' + Cast (Error_State() as nvarchar);
+  		Print Replicate('-',40)
+	End Catch
+End
